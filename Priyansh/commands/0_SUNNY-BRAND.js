6109d6f630796723f366fa230244ff140e7de19c
AADI BABU <ramswaroop67363@gmail.com>
2024-12-02 21:59:04 +0530

    Update commands

---
+++ Priyansh/commands/0_SUNNY-BRAND.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+        name: "sunny",
+    version: "1.0.1",
+        hasPermssion: 0,
+        credits: "AADI BABU", 
+        description: "hihihihi",
+        commandCategory: "no prefix",
+        usages: "sunny",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+        var { threadID, messageID } = event;
+        if (event.body.indexOf("@Sunny Brand ")==0 || event.body.indexOf("sunny")==0 || event.body.indexOf("Sunny")==0 || event.body.indexOf("SUNNY")==0) {
+                var msg = {
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻   \n_______________________\n\n. 𝐎𝐰𝐧𝐞𝐫 ==∆ SuNNY \n_______________________",
+                                attachment: fs.createReadStream(__dirname + `/noprefix/IMG_20241202_205516_489.webp`)
+                        }
+                        api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😎", event.messageID, (err) => {}, true)
+                }
+        }
+        module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+        }
