e2ea2df4ef777feeb162e594684439c640b58ca5
AADI BABU <ramswaroop67363@gmail.com>
2024-12-04 07:52:21 +0530

    Update 0_AADI.js

--- Priyansh/commands/0_AADI.js
+++ Priyansh/commands/0_AADI.js
@@ -1,29 +1,53 @@
- const fs = require("fs");
-module.exports.config = {
-	name: "aadi",
-    version: "1.0.1",
-	hasPermssion: 0,
-	credits: "Arun", 
-	description: "no prefix",
-	commandCategory: "No command marks needed",
-	usages: "...",
-    cooldowns: 100, 
-};
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	let react = event.body.toLowerCase();
-	if(react.includes("Owner") ||
-     react.includes("OWNER") || 
-react.includes("owner")) {
-		var msg = {
-				body: "𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐓𝐨 𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 𝐁𝐨𝐭 😇👈●============================================================●\n\n  =𝐎𝐰𝐧𝐞𝐫 ➻    🌹 𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 🌹\n●============================================================●\n𝐅𝐎𝐑 𝐀𝐍𝐘 𝐊𝐈𝐍𝐆 𝐎𝐅𝐅 𝐇𝐄𝐋𝐏 𝐂𝐎𝐍𝐓𝐀𝐂𝐓 𝐎𝐍 𝐈𝐍𝐒𝐓𝐀 𝐈𝐃\n●============================================================● https://www.instagram.com/aadi_singh__143?igsh=MXVneXZ6bzR4bHhvcg==\n●============================================================●\n𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝 𝐥𝐢𝐧𝐤 😊 𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 :- ☞\n𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 :- ☞ 𝐊𝐀 𝐋𝐔𝐊 😁😋●============================================================● https://www.facebook.com/profile.php?id=100066401546757&mibextid=ZbWKwL`",
-				attachment: fs.createReadStream(__dirname + `/noprefix/FB_IMG_1710371802935.jpg`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("📷", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
+module.exports = {
+    config: {
+        name: "owner",
+        version: "1.0.1",
+        hasPermssion: 0,
+        credits: "AADI BABU",
+        description: "no prefix",
+        usePrefix: false,
+        commandCategory: "No command marks needed",
+        usages: "Yo Yo",
+        cooldowns: 5,
+    },
 
-  }
+    // Array of Imgur URLs
+    handleEvent: async function({ api, event, client, Users, __GLOBAL }) {
+        const gifs = [
+            "https://i.imgur.com/HI2QPof.jpeg",
+            "https://i.imgur.com/GmwtlyW.jpeg",
+            "https://i.imgur.com/f8xCMLp.jpeg",
+            "https://i.imgur.com/jBdCIce.jpeg",
+            "https://i.imgur.com/MJrbrDW.jpeg",
+            "https://i.imgur.com/ItvIrPx.jpeg"
+        ];
+
+        const message = "𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐓𝐨 𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 𝐁𝐨𝐭 😇👈\n──────────────────\n  =𝐎𝐰𝐧𝐞𝐫 ➻    🌹 𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 🌹\n──────────────────\n𝐅𝐎𝐑 𝐀𝐍𝐘 𝐊𝐈𝐍𝐆 𝐎𝐅𝐅 𝐇𝐄𝐋𝐏 𝐂𝐎𝐍𝐓𝐀𝐂𝐓 𝐎𝐍 𝐈𝐍𝐒𝐓𝐀 𝐈𝐃\n──────────────────\nhttps://www.instagram.com/aadi_singh__143?igsh=MXVneXZ6bzR4bHhvcg==\n──────────────────\n𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝 𝐥𝐢𝐧𝐤 😊 𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 :- ☞\n𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 :- ☞ 𝐊𝐀 𝐋𝐔𝐊 😁 \n──────────────────\nhttps://www.facebook.com/profile.php?id=100066401546757&mibextid=ZbWKwL";
+        const { threadID, messageID } = event;
+        const lowerCaseMessage = event.body.toLowerCase();
+
+        if (lowerCaseMessage.startsWith("Owner")) {  
+            // Select a random image URL from the array
+            const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
+            const downloadPath = path.join(__dirname, 'owner-Jpg-Images.jpg');
+
+            // Download image from the random URL
+            request(randomGif).pipe(fs.createWriteStream(downloadPath)).on('close', () => {
+                const msg = {
+                    body: message,
+                    attachment: fs.createReadStream(downloadPath)
+                };
+                api.sendMessage(msg, threadID, messageID);
+                api.setMessageReaction("😘", event.messageID, (err) => {}, true);
+            });
+        }
+    },
+
+    run: function({ api, event, client, __GLOBAL }) {
+        // Empty run function for module requirements
+    },
+}
