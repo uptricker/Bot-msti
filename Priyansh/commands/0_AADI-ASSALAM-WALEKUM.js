01265204ab1727ee763e2fe6ecb80c2f6066bee6
AADI BABU <ramswaroop67363@gmail.com>
2024-12-05 11:51:38 +0530

    Update 0_AADI-ASSALAM-WALEKUM.js

--- Priyansh/commands/0_AADI-ASSALAM-WALEKUM.js
+++ Priyansh/commands/0_AADI-ASSALAM-WALEKUM.js
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("Assalam alaikum") ||
 react.includes("assalamualaikum")) {
                 var msg = {
-                                body: `_______________________\n\nWalekum Aaslamu  Wa Ramutllha Barkatu 😇\n_______________________\n𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐀𝐚̃𝐃𝐢   𝐁𝐚𝐁𝐮`,
+                                body: `──────────────────\n\nWalekum Aaslamu  Wa Ramutllha Barkatu 😇\n──────────────────\n𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐀𝐚̃𝐃𝐢   𝐁𝐚𝐁𝐮`,
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🤲", event.messageID, (err) => {}, true)
