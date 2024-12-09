d15f1a9b0e04ee24b36110e128b0db97fd4bd840
Botjanu4322 <botjanu668@gmail.com>
2024-12-09 13:44:03 +0530

    Update and rename 0_AADI_GOLGAPPE.js to 0_ARYAN_GOLGAPPE.js

--- Priyansh/commands/0_ARYAN_GOLGAPPE.js
+++ Priyansh/commands/0_ARYAN_GOLGAPPE.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "GOLGAPPE",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+	credits: "ARYAN", 
 	description: "hihihihi",
 	commandCategory: "no prefix",
 	usages: "sub",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("Golgappe")==0 || event.body.indexOf("Golgappa")==0 || event.body.indexOf("Pani puri")==0 || event.body.indexOf("PANI PURI")==0) {
 		var msg = {
-				body: "𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐀𝐚̃𝐃𝐢   𝐁𝐚𝐁𝐮 \n 🩷 𝐘𝐀 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐆𝐎𝐋𝐆𝐀𝐏𝐏𝐀 𝐊𝐇𝐀𝐎 🩷",
+				body: "𝐎𝐰𝐧𝐞𝐫 ➻  ────  💐𝐀𝐑𝐘𝐀𝐍💐\n 🩷 𝐘𝐀 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐆𝐎𝐋𝐆𝐀𝐏𝐏𝐀 𝐊𝐇𝐀𝐎 🩷",
 				attachment: fs.createReadStream(__dirname + `/noprefix/Pin_on_Pins_by_you(360P)(1).mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
