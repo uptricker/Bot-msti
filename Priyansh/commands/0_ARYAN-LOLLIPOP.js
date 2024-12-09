f43692bb90ee4268f8f2ba673e8533c920797977
Botjanu4322 <botjanu668@gmail.com>
2024-12-09 13:29:55 +0530

    Update and rename 0_AADI-LOLLIPOP.js to 0_ARYAN-LOLLIPOP.js

--- Priyansh/commands/0_ARYAN-LOLLIPOP.js
+++ Priyansh/commands/0_ARYAN-LOLLIPOP.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "lolipop",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "ARYAN", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "lolopop",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("lolipop")==0 || event.body.indexOf("LOLIPOP")==0 || event.body.indexOf("Lolipop ")==0 || event.body.indexOf("lolipopp")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐋𝐎𝐋𝐈𝐏𝐎𝐏\n_______________________",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐀𝐑𝐘𝐀𝐍💐 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐋𝐎𝐋𝐈𝐏𝐎𝐏\n_______________________",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/8ae90a3cec3e329941aa87a4c6cacb34.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
