a7685c1db7d4e5f33fc136018c3890e54ee0aadd
Botjanu4322 <botjanu668@gmail.com>
2024-12-09 13:32:29 +0530

    Update and rename 0_AADI-PARATHE.js to 0_ARYAN-PARATHE.js

--- Priyansh/commands/0_ARYAN-PARATHE.js
+++ Priyansh/commands/0_ARYAN-PARATHE.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "parathe",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "ARYAN", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "parathe",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("parathe")==0 || event.body.indexOf("paratha")==0 || event.body.indexOf("PARATHE")==0 || event.body.indexOf("Parathe")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐚𝐑𝐚𝐓𝐡𝐞\n_______________________",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐀𝐑𝐘𝐀𝐍💐 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐚𝐑𝐚𝐓𝐡𝐞\n_______________________",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/7688e35b3d9376764427668db625967c.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
