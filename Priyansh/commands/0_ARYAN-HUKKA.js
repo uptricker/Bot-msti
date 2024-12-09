3a0644c066b47bb0a416f396d7fc0d23a6f924cd
Botjanu4322 <botjanu668@gmail.com>
2024-12-09 13:25:07 +0530

    Update and rename 0_AADI-HUKKA.js to 0_ARYAN-HUKKA.js

--- Priyansh/commands/0_ARYAN-HUKKA.js
+++ Priyansh/commands/0_ARYAN-HUKKA.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "hukka",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "ARYAN", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "hukka",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("hukka")==0 || event.body.indexOf("Hukka")==0 || event.body.indexOf("HUKKA")==0 || event.body.indexOf("Hukkaa ")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐇𝐮𝐊𝐊𝐚\n_______________________",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐀𝐑𝐘𝐀𝐍💐\n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐇𝐮𝐊𝐊𝐚\n_______________________",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/SheSha_Snap_💀_in_2024_|_Cigratte_snapchat,_Snap_streak_ideas_easy,_Night_club_aesthetic(360P).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
