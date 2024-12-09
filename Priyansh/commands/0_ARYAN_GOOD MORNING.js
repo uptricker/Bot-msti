7332718c2d953e2bd7c7287810bdaf7182acdedf
Botjanu4322 <botjanu668@gmail.com>
2024-12-09 13:44:51 +0530

    Update and rename 0_AADI_GOOD MORNING.js to 0_ARYAN_GOOD MORNING.js

--- Priyansh/commands/0_ARYAN_GOOD MORNING.js
+++ Priyansh/commands/0_ARYAN_GOOD MORNING.js
@@ -3,10 +3,10 @@ module.exports.config = {
 	name: "GOOD MORNING",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "AADI BABU", 
+	credits: "ARYAN", 
 	description: "hihihihi",
 	commandCategory: "no prefix",
-	usages: "arif",
+	usages: "morning",
     cooldowns: 5, 
 };
 
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("GM")==0 || event.body.indexOf("Gm")==0 || event.body.indexOf("Good morning")==0 || event.body.indexOf("GOOD MORNING")==0) {
 		var msg = {
-				body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n_______________________\n\n⎯꯭֯🌸⃪ ꯭⃛֯G❍❍𝐃 ɱ❍ɽηIIηG\n_______________________",
+				body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐀𝐑𝐘𝐀𝐍💐 \n_______________________\n\n⎯꯭֯🌸⃪ ꯭⃛֯G❍❍𝐃 ɱ❍ɽηIIηG\n_______________________",
 				attachment: fs.createReadStream(__dirname + `/noprefix/received_1083155713406602.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
