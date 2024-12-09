7c3a5eaf30a2ab8934c29a60a16b83f336b4fe88
Botjanu4322 <botjanu668@gmail.com>
2024-12-09 13:31:30 +0530

    Update 0.js

--- Priyansh/commands/0.js
+++ Priyansh/commands/0.js
@@ -3,8 +3,8 @@ module.exports.config = {
 	name: "GANA SONG",
     version: "1.1.1",
 	hasPermssion: 0,
-	credits: "AADI BABU", 
-	description: "THIS BOT IS MR AADI BABU",
+	credits: "ARYAN", 
+	description: "THIS BOT IS MR ARYAN",
 	commandCategory: "no prefix",
     cooldowns: 5, 
 };
