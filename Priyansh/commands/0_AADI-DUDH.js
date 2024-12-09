57232cb08fbaf39652869e17c839cafc93ff0221
Botjanu4322 <botjanu668@gmail.com>
2024-12-09 13:20:09 +0530

    Update 0_AADI-DUDH.js

--- Priyansh/commands/0_AADI-DUDH.js
+++ Priyansh/commands/0_AADI-DUDH.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "dudh",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "ARYAN", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "dudh",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("dudh")==0 || event.body.indexOf("DUDH")==0 || event.body.indexOf("DHUD")==0 || event.body.indexOf("Dudh")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐃𝐮𝐝𝐇 \n_______________________",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻ 💐𝐀𝐑𝐘𝐀𝐍💐 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐃𝐮𝐝𝐇 \n_______________________",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/(720p).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
