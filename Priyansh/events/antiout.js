ef3ba3f31825601398e37f479fd2ff4de1f1d9f9
Botjanu4322 <botjanu668@gmail.com>
2024-12-10 10:57:33 +0530

    Update antiout.js

--- Priyansh/events/antiout.js
+++ Priyansh/events/antiout.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antiout",
  eventType: ["log:unsubscribe"],
  version: "0.0.1",
- credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+ credits: "ARYAN",
  description: "Listen events"
 };
 
@@ -15,8 +15,8 @@ module.exports.run = async({ event, api, Threads, Users }) => {
  if (type == "self-separation") {
   api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
    if (error) {
-    api.sendMessage(`Isse Dubara Add Nhi Kar Paya 🥺 ${name} Group Mai :( `, event.threadID)
-   } else api.sendMessage(`Bhag Ke Jaane Ka Nhi, ${name} Baby, Dekho Phir Se Add Kardiya Aapko`, event.threadID);
+    api.sendMessage(`💐𝐁𝐎𝐒𝐒 𝐌 𝐈𝐒𝐄 𝐃𝐔𝐁𝐀𝐑𝐀 𝐀𝐃𝐃 𝐍𝐀𝐇𝐈 𝐊𝐀𝐑 𝐏𝐀𝐘𝐀💐 ${name} 𝐆𝐑𝐎𝐔𝐏 𝐌 :( `, event.threadID)
+   } else api.sendMessage(`💐𝐀𝐋𝐄 𝐌𝐄𝐑𝐈 𝐉𝐀𝐀𝐍 𝐁𝐇𝐀𝐆 𝐊𝐀𝐑 𝐊𝐀𝐇𝐀 𝐉𝐀 𝐑𝐀𝐇𝐄 𝐇𝐎💐, ${name} 💐𝐇𝐔𝐌 𝐀𝐏𝐊𝐎 𝐁𝐇𝐀𝐆𝐍𝐄 𝐍𝐀𝐇𝐈 𝐃𝐄𝐍𝐆𝐄 𝐉𝐀𝐀𝐍,𝐃𝐄𝐊𝐇𝐎 𝐉𝐀𝐀𝐍 𝐌𝐄𝐍𝐄 𝐀𝐏𝐊𝐎 𝐏𝐇𝐈𝐑 𝐀𝐃𝐃 𝐊𝐀𝐑 𝐃𝐈𝐘𝐀😘💐`, event.threadID);
   })
  }
 }
