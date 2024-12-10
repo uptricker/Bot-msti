9aa7bccdad0906463f06fb72e12c31814c40e7ca
Botjanu4322 <botjanu668@gmail.com>
2024-12-10 13:07:17 +0530

    Update antiout.js

--- Priyansh/events/antiout.js
+++ Priyansh/events/antiout.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antiout",
  eventType: ["log:unsubscribe"],
  version: "0.0.1",
- credits: "ARYAN",
+ credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
  description: "Listen events"
 };
 
