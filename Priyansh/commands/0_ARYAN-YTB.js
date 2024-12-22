6e6c1d8497b565e289abc7002d6d991804632a3b
NITYA-ARYAN <rrjput768@gmail.com>
2024-12-22 15:30:48 +0530

    Update 0_ARYAN-YTB.js

--- Priyansh/commands/0_ARYAN-YTB.js
+++ Priyansh/commands/0_ARYAN-YTB.js
@@ -1,4 +1,4 @@
-.cmd install ytb.js const axios = require("axios");
+ const axios = require("axios");
 const fs = require("fs-extra");
 const path = require("path");
 const ytdl = require("@distube/ytdl-core");
