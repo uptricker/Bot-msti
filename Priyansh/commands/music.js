38657112d01db256aa8d3dc3890cca8f26851b22
NITYA-ARYAN <rrjput768@gmail.com>
2024-12-24 09:31:34 +0530

    Update music.js

--- Priyansh/commands/music.js
+++ Priyansh/commands/music.js
@@ -80,7 +80,7 @@ module.exports = {
       // Save the song file locally
       fs.writeFileSync(downloadPath, songBuffer);
 
-      api.setMessageReaction("✅", event.messageID, () => {}, true);
+      api.setMessageReaction("🩵", event.messageID, () => {}, true);
 
       await api.sendMessage(
         {
