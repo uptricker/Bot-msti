475b853603c7fa5bfc88d2d61f6c1024b7dac8f5
Shaanbot1234 <b8465391@gmail.com>
2024-12-30 10:04:18 +0500

    Update 0_ARYAN-GULABJAMUN.js

--- Priyansh/commands/0_ARYAN-GULABJAMUN.js
+++ Priyansh/commands/0_ARYAN-GULABJAMUN.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("julabjamun")==0 || event.body.indexOf("GULABJAMUN")==0 || event.body.indexOf("Gulabjamun")==0 || event.body.indexOf("sargulla")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐀𝐑𝐘𝐀𝐍💐 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐆𝐮𝐥𝐚𝐁𝐣𝐚𝐌𝐮𝐧 \n_______________________",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐒𝐇𝐀𝐀𝐍💐 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐆𝐮𝐥𝐚𝐁𝐣𝐚𝐌𝐮𝐧 \n_______________________",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/Pinterest-Foodie-Heaven-Dessert-Recipe-Cake-Recipe-Cooking-Tips-Gulab-Jamun-Recipe.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
