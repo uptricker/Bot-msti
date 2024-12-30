d92dc8a6db085eb4ddc56cf0d90ec68a4825d683
Shaanbot1234 <b8465391@gmail.com>
2024-12-30 10:06:57 +0500

    Update 0_ARYAN-CHAUMIN.js

--- Priyansh/commands/0_ARYAN-CHAUMIN.js
+++ Priyansh/commands/0_ARYAN-CHAUMIN.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("chaumin")==0 || event.body.indexOf("Chaumin")==0 || event.body.indexOf("CHAUMIN")==0 || event.body.indexOf("chawmin")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻ 💐𝐀𝐑𝐘𝐀𝐍💐\n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐜𝐇𝐚𝐔𝐦𝐈𝐧 \n_______________________",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻ 💐𝐒𝐇𝐀𝐀𝐍💐\n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐜𝐇𝐚𝐔𝐦𝐈𝐧 \n_______________________",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/8a9f84ed741b6df2854fb136dd028e79.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
