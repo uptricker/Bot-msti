83d795633d9aade06ee10234b9ec630d9a1d395b
Shaanbot1234 <b8465391@gmail.com>
2024-12-30 10:08:42 +0500

    Update 0_ARYAN-ASSALAM-WALEKUM.js

--- Priyansh/commands/0_ARYAN-ASSALAM-WALEKUM.js
+++ Priyansh/commands/0_ARYAN-ASSALAM-WALEKUM.js
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("Assalam alaikum") ||
 react.includes("assalamualaikum")) {
                 var msg = {
-                                body: `──────────────────\n\nWalekum Aaslamu  Wa Ramutllha Barkatu 😇\n──────────────────\n𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐀𝐑𝐘𝐀𝐍💐─`,
+                                body: `──────────────────\n\nWalekum Aaslamu  Wa Ramutllha Barkatu 😇\n──────────────────\n𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐒𝐇𝐀𝐀𝐍💐─`,
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🤲", event.messageID, (err) => {}, true)
