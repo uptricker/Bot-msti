9e0db5e517220275d553084633e07e8eae5d1b30
Shaanbot1234 <b8465391@gmail.com>
2024-12-30 10:01:25 +0500

    Update 0_ARYAN-LOLLIPOP.js

--- Priyansh/commands/0_ARYAN-LOLLIPOP.js
+++ Priyansh/commands/0_ARYAN-LOLLIPOP.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("lolipop")==0 || event.body.indexOf("LOLIPOP")==0 || event.body.indexOf("Lolipop ")==0 || event.body.indexOf("lolipopp")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐀𝐑𝐘𝐀𝐍💐 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐋𝐎𝐋𝐈𝐏𝐎𝐏\n_______________________",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐒𝐇𝐀𝐀𝐍💐 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐋𝐎𝐋𝐈𝐏𝐎𝐏\n_______________________",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/8ae90a3cec3e329941aa87a4c6cacb34.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
