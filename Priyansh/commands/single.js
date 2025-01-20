1da2cbdfdd83100b140bb011d339b0d641730394
Shaan Personal bat <b8465391@gmail.com>
2025-01-20 13:00:33 +0500

    Update commands

---
+++ Priyansh/commands/single.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "Shaan",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "Amir", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "npxs5",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("single")==0 || event.body.indexOf("Single")==0 || event.body.indexOf("single")==0 || event.body.indexOf("single")==0) {
+		var msg = {
+				body: "𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍 𝐊 owner",
+				attachment: fs.createReadStream(__dirname + `/noprefix/Single.mp4`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("💗", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
