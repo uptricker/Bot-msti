07d835f73c2a168869eb77d57136d128e484c814
Shaan Personal bat <b8465391@gmail.com>
2025-01-26 22:51:04 +0500

    Update began.js

--- Priyansh/commands/began.js
+++ Priyansh/commands/began.js
@@ -12,10 +12,10 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Ayen")==0 || event.body.indexOf("ayeen🙄")==0 || event.body.indexOf("Baigan")==0 || event.body.indexOf("Began")==0) {
+	if (event.body.indexOf("Ayen")==0 || event.body.indexOf("🙄🙄🙄")==0 || event.body.indexOf("Baigan")==0 || event.body.indexOf("Began")==0) {
 		var msg = {
 				body: "🤭Ayeein🤔🤔🙄",
-				attachment: fs.createReadStream(__dirname + `/noprefix/VID_20250126223024.mp4`)
+				attachment: fs.createReadStream(__dirname + `/noprefix/began.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🤫", event.messageID, (err) => {}, true)
