416835edc7a1c7861b7f763c5a7bb194a84459b3
Shaan Personal bat <b8465391@gmail.com>
2025-01-28 09:47:55 +0500

    Create pyarisamjhgayi.js

---
+++ Priyansh/commands/pyarisamjhgayi.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "PyariSamjhGayi",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "MrTomXxX", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "began ayee",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("🙈")==0 || event.body.indexOf("🙈🙈")==0 || event.body.indexOf("Pyari")==0 || event.body.indexOf("🙈")==0) {
+		var msg = {
+				body: "🤭Pyari Samjh gyi 🫠",
+				attachment: fs.createReadStream(__dirname + `/noprefix/received_605892652397011.mp4`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🤫", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
