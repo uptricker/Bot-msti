483c721e68676292e2b914584c92d782a02b32b3
Shaan Personal bat <b8465391@gmail.com>
2025-03-15 21:51:53 +0500

    Update rankup.js

--- Priyansh/commands/rankup.js
+++ Priyansh/commands/rankup.js
@@ -122,7 +122,8 @@ module.exports.languages = {
 		"on": "on",
 		"off": "off",
 		"successText": "success notification rankup!",
-		"levelup": "{name}, apka ek aur levelup ho geya hai  level {level}",
+		"levelup": "{name}, apka ek aur levelup ho geya hai  level
+◈━━━━━━━━━━━━━━━━💚✨\n\n{name}, ◈━━━━━━━━━━━━━━━━💚✨\n\nyour keyboard has reached level {level}◈━━━━━━━━━━━━━━━━💚✨\n\𝐌𝐀𝐃𝐄 𝐁𝐘 𝐒𝐇𝐀𝐀𝐍\n◈━━━━━━━━━━━━━━━━💚✨ {level}",
 	}
 }
 
