d48c7bfc6f1fd867fd113b7b24d4f1acffb119ac
innocent-uzair <uzairrajputemoji9@gmail.com>
2025-07-02 11:13:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const { spawn } = require("child_process");
 const axios = require("axios");
 const logger = require("./utils/log");
+const express = require('express');
+const path = require('path');
+const fs = require("fs");
 
 ///////////////////////////////////////////////////////////
 //========= Create website for dashboard/uptime =========//
 ///////////////////////////////////////////////////////////
 
-const express = require('express');
-const path = require('path');
-
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Serve the index.html file
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿.html'));
 });
 
-// Start the server and add error handling
 app.listen(port, () => {
     logger(`Server is running on port ${port}...`, "[ Starting ]");
 }).on('error', (err) => {
@@ -32,13 +30,12 @@ app.listen(port, () => {
 //========= Create start bot and make it loop =========//
 /////////////////////////////////////////////////////////
 
-// Initialize global restart counter
 global.countRestart = global.countRestart || 0;
 
 function startBot(message) {
     if (message) logger(message, "[ Starting ]");
 
-    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "Priyansh.js"], {
+    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
@@ -57,21 +54,7 @@ function startBot(message) {
     child.on("error", (error) => {
         logger(`An error occurred: ${JSON.stringify(error)}`, "[ Error ]");
     });
-};
-
-////////////////////////////////////////////////
-//========= Check update from Github =========//
-////////////////////////////////////////////////
-
-axios.get("https://raw.githubusercontent.com/priyanshu192/bot/main/package.json")
-    .then((res) => {
-        logger(res.data.name, "[ NAME ]");
-        logger(`Version: ${res.data.version}`, "[ VERSION ]");
-        logger(res.data.description, "[ DESCRIPTION ]");
-    })
-    .catch((err) => {
-        logger(`Failed to fetch update info: ${err.message}`, "[ Update Error ]");
-    });
+}
 
 // Start the bot
 startBot();
