5163607b006b5fa67ddfe8ebb2f4fc54f0fda5d6
sushilbabu <premyadav109q@gmail.com>
2025-07-11 00:05:25 +0500

    Update 0_ARYAN_HOT-VIDEO.js

--- Priyansh/commands/0_ARYAN_HOT-VIDEO.js
+++ Priyansh/commands/0_ARYAN_HOT-VIDEO.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "Hotvideos",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "ARYAN",
+  credits: "SUSHIL",
   description: "HOT VEDIO",
   commandCategory: "system",
   usages: "Hotvideos",
@@ -19,7 +19,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["『٭』 💐𝐒𝐇𝐀𝐀𝐍💐 『٭』-HOT-VIDOS"];
+   var hi = ["『٭』 💐𝐒𝐔𝐒𝐇𝐈𝐋💐 『٭』-HOT-VIDOS"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
    "https://drive.google.com/uc?id=1a7XsNXizFTTlSD_gRQwK4bDA3HPam56W",
