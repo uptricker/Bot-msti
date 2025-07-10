961d6f076bb59544bdc97b172a13d8ce4bf0a7bf
sushilbabu <premyadav109q@gmail.com>
2025-07-10 23:45:02 +0500

    Update 0_ARYAN-JUICES.js

--- Priyansh/commands/0_ARYAN-JUICES.js
+++ Priyansh/commands/0_ARYAN-JUICES.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "juices",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "ARYAN", 
+        credits: "SUSHIL", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "juice",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("juice")==0 || event.body.indexOf("juices")==0 || event.body.indexOf("Juice")==0 || event.body.indexOf("JUICE")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐒𝐇𝐀𝐀𝐍💐 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐉𝐮𝐢𝐂𝐞\n_______________________",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐒𝐔𝐒𝐇𝐈𝐋💐 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐉𝐮𝐢𝐂𝐞\n_______________________",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/Never_forget_to_hydrate_your_body_every_day🥤🍓😋_|_Gıda,_Smoothies,_Çilekler(480P).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
