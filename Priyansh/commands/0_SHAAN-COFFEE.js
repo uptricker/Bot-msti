64d35988e3b2ec1afd1c9d87c6b52bad25c232ef
sushilbabu <premyadav109q@gmail.com>
2025-07-11 00:19:53 +0500

    Update 0_SHAAN-COFFEE.js

--- Priyansh/commands/0_SHAAN-COFFEE.js
+++ Priyansh/commands/0_SHAAN-COFFEE.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "Coffee",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "SHAAN", 
+        credits: "SUSHIL", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "Coffee",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("Coffee")==0 || event.body.indexOf("coffee")==0 || event.body.indexOf("COFFEE ")==0 || event.body.indexOf("cofi")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻ 💐𝐒𝐇𝐀𝐀𝐍💐\n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐎𝐅𝐅𝐄𝐄 \n_______________________",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻ 💐𝐒𝐔𝐒𝐇𝐈𝐋💐\n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐎𝐅𝐅𝐄𝐄 \n_______________________",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/Pinterest-boomerang.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
