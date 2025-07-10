5ad2e0337f8d10006a8f1e52b0c6e6386f0342f9
sushilbabu <premyadav109q@gmail.com>
2025-07-10 23:48:33 +0500

    Update 0_ARYAN-PIZZA.js

--- Priyansh/commands/0_ARYAN-PIZZA.js
+++ Priyansh/commands/0_ARYAN-PIZZA.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "pizza",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "ARYAN", 
+        credits: "SUSHIL", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "pizza",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("pizza")==0 || event.body.indexOf("Pizza")==0 || event.body.indexOf("PIZZA")==0 || event.body.indexOf("PIZZAA")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐒𝐇𝐀𝐀𝐍💐\n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐈𝐙𝐙𝐀 \n_______________________",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐒𝐔𝐒𝐇𝐈𝐋💐\n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐈𝐙𝐙𝐀 \n_______________________",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/Pinterest-Abhijeet-Agnihotri-pizza.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
