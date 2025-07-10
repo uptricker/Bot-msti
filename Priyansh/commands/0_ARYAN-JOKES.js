039ec9aa7f428b6e4f8cd3a9142637b462dbc577
sushilbabu <premyadav109q@gmail.com>
2025-07-10 23:44:35 +0500

    Update 0_ARYAN-JOKES.js

--- Priyansh/commands/0_ARYAN-JOKES.js
+++ Priyansh/commands/0_ARYAN-JOKES.js
@@ -4,7 +4,7 @@ module.exports.config = {
   name: "joke",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "ARYAN",
+  credits: "SUSHIL",
   description: "Ek mazedaar joke bhejta hai",
   commandCategory: "Fun",
   usages: "joke",
