6f2ababf98820bfc17b66b39c26c12e9e6713204
sushilbabu <premyadav109q@gmail.com>
2025-07-10 23:48:01 +0500

    Update 0_ARYAN-PEPSI.js

--- Priyansh/commands/0_ARYAN-PEPSI.js
+++ Priyansh/commands/0_ARYAN-PEPSI.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "pepsi",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "ARYAN", 
+        credits: "SUSHIL", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "pepsi",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("pepsi")==0 || event.body.indexOf("Pepsi")==0 || event.body.indexOf("PEPSI")==0 || event.body.indexOf("Coldaring")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻ 💐𝐒𝐇𝐀𝐀𝐍💐\n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐄𝐏𝐒𝐈\n_______________________",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻ 💐𝐒𝐔𝐒𝐇𝐈𝐋💐\n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐄𝐏𝐒𝐈\n_______________________",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/Pin_di_Shoaib_nazir_su_Quick_Saves(360P).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
