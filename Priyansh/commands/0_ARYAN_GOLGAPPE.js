ace705d9c13480194b5d1438b61c1bc5d0267c82
sushilbabu <premyadav109q@gmail.com>
2025-07-11 00:03:22 +0500

    Update 0_ARYAN_GOLGAPPE.js

--- Priyansh/commands/0_ARYAN_GOLGAPPE.js
+++ Priyansh/commands/0_ARYAN_GOLGAPPE.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "GOLGAPPE",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "ARYAN", 
+	credits: "SUSHIL", 
 	description: "hihihihi",
 	commandCategory: "no prefix",
 	usages: "sub",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("Golgappe")==0 || event.body.indexOf("Golgappa")==0 || event.body.indexOf("Pani puri")==0 || event.body.indexOf("PANI PURI")==0) {
 		var msg = {
-				body: "𝐎𝐰𝐧𝐞𝐫 ➻  ────  💐𝐒𝐇𝐀𝐀𝐍💐\n 🩷 𝐘𝐀 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐆𝐎𝐋𝐆𝐀𝐏𝐏𝐀 𝐊𝐇𝐀𝐎 🩷",
+				body: "𝐎𝐰𝐧𝐞𝐫 ➻  ────  💐𝐒𝐔𝐒𝐇𝐈𝐋💐\n 🩷 𝐘𝐀 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐆𝐎𝐋𝐆𝐀𝐏𝐏𝐀 𝐊𝐇𝐀𝐎 🩷",
 				attachment: fs.createReadStream(__dirname + `/noprefix/Pin_on_Pins_by_you(360P)(1).mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
