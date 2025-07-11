ef21697d11189cdd478bff3b51070e1014d3e58f
sushilbabu <premyadav109q@gmail.com>
2025-07-11 11:44:05 +0500

    Update kick.js

--- Priyansh/commands/kick.js
+++ Priyansh/commands/kick.js
@@ -17,7 +17,7 @@ module.exports.languages = {
 	},
 	"en": {
 		"error": "Error! An error occurred. Please try again later!",
-		"needPermssion": "Are Shaan Yar Pehle group admin To Karo Bot Ko\nFir Kick Karo Na 😂😂!",
+		"needPermssion": "Are Sushil Yar Pehle group admin To Karo Bot Ko\nFir Kick Karo Na 😂😂!",
 		"missingTag": "You need tag some person to kick"
 	}
 }
