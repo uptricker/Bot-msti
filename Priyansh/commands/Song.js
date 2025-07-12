ca3d602cd830b14fe981a2fb798b00b95a8f9018
sushilbabu <premyadav109q@gmail.com>
2025-07-12 07:26:24 +0500

    Update Song.js

--- Priyansh/commands/Song.js
+++ Priyansh/commands/Song.js
@@ -75,7 +75,7 @@ module.exports.run = async function ({ api, event, args }) {
 
     writer.on("finish", async () => {
       await api.sendMessage({
-        body: `🎶 |  »»𝑶𝑾𝑵𝑬𝑹««★™  »»𝑺𝑯𝑨𝑨𝑵 𝑲𝑯𝑨𝑵««
+        body: `🎶 |  »»𝑶𝑾𝑵𝑬𝑹««★™  »»𝐒𝐔𝐒𝐇𝐈𝐋 𝐘𝐀𝐃𝐀𝐕««
           🥀𝒀𝑬 𝑳𝑶 𝑩𝑨𝑩𝒀 𝑨𝑷𝑲𝑰 𝑺𝑶𝑵𝑮👉: "${title}"`,
         attachment: fs.createReadStream(filePath)
       }, event.threadID);
